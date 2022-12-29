cea14fae0ef5c9984bfbde48211f7a9f254a0a34
Davide-Console <davideconsole99@gmail.com>
2022-12-29 22:31:31 +0100

    noise detection

--- noise_detection.py
+++ noise_detection.py
@@ -1,8 +1,3 @@
-from tqdm import tqdm
-from math import log10, sqrt
-import os
-import numpy as np
-import matplotlib.pyplot as plt
 import tensorflow as tf
 image=tf.keras.preprocessing.image
 from dataset_utils import *
@@ -10,36 +5,6 @@ import cv2
 from skimage import filters
 from skimage.measure import label
 
-def getLargestCC(segmentation):
-    labels = label(segmentation)
-    assert (labels.max() != 0)  # assume at least 1 CC
-    largestCC = labels == np.argmax(np.bincount(labels.flat)[1:]) + 1
-    return largestCC
-
-
-def get_body_mask(img):
-    val = filters.threshold_otsu(img)
-
-    mask = (img > val) * 1.0
-
-    mask = getLargestCC(mask)
-
-#    inv_mask = np.zeros((len(mask), len(mask)), dtype=int)
- #   for x in range(len(inv_mask)):
-  #      for y in range(len(inv_mask)):
-   #         if mask[x, y] == 0:
-    #            inv_mask[x, y] = 1
-
-#    inv_mask[0:255, :] = getLargestCC(inv_mask[0:255, :])
- #   inv_mask[255:511, :] = getLargestCC(inv_mask[255:511, :])
-
-#    for x in range(len(inv_mask)):
- #       for y in range(len(inv_mask)):
-  #          if inv_mask[x, y] == 0:
-   #             mask[x, y] = 1
-    #        elif inv_mask[x, y] == 1:
-     #           mask[x, y] = 0
-    return mask
 
 if __name__ == '__main__':
     seed = 1
@@ -54,14 +19,14 @@ if __name__ == '__main__':
     count_inverted=0
     count_normal=0
     count_sp=0
-    count_gaussian=0
+    count_uniform=0
     for i in range(len(patients)):
         img = cv2.imread('dataset/'+patients[i], 0)
         img = cv2.resize(img, (256, 256), interpolation=cv2.INTER_CUBIC)
 
         val = filters.threshold_otsu(img)
         mask = (img > val) * 1.0
-
+        total_hist, x = np.histogram(img, bins=256, range=(0, 255))
         histBR, x = np.histogram(mask[230:256, 230:256], bins=2, range=(0, 1))
         histTL, x = np.histogram(mask[0:25, 0:25], bins=2, range=(0, 1))
         histTR, x = np.histogram(mask[0:25, 230:256], bins=2, range=(0, 1))
@@ -70,12 +35,26 @@ if __name__ == '__main__':
         labels = label(mask)
 
         if len(np.unique(labels)) >= 100:
-            print(patients[i], '\tGAUSSIAN (MASK)')
-            count_gaussian += 1
+            uniform=True
+            for j in range(254):
+                if total_hist[j + 1] >= sum(total_hist[1:255]) / 254 * 1.5 or \
+                        total_hist[j + 1] <= sum(total_hist[1:255]) / 254 * 0.5:
+                    uniform = False
+            if uniform is True:
+                print(patients[i], '\tUNIFORM NOISE')
+                count_uniform += 1
+            elif uniform is False:
+                print(patients[i], '\tS&P NOISE')
+                count_sp += 1
         else:
             if hist[0] > hist[1]:
-                print(patients[i], '\tNORMAL (MASK)')
+                print(patients[i], '\tNORMAL')
                 count_normal += 1
             else:
-                print(patients[i], '\tINVERTED (MASK)')
+                print(patients[i], '\tINVERTED')
                 count_inverted += 1
+
+    print('Normal images:\t', round(count_normal/len(patients), 2), '%')
+    print('Inverted images:\t', round(count_inverted/len(patients), 2), '%')
+    print('Uniform images:\t', round(count_uniform/len(patients), 2), '%')
+    print('S&P images:\t', round(count_sp/len(patients), 2), '%')
